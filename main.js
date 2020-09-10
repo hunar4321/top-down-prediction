e34ec2996ab9e61075a42da40cd70bcb259dc7dd
Hunar Ahmad <39065447+hunar4321@users.noreply.github.com>
2020-09-10 22:59:14 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -8,7 +8,6 @@ mypat=[];
 score=[];
 shift=40;
 hide=true;
-trials=8;
 state=0;
 level=1;
 
@@ -24,6 +23,9 @@ mainX = 290
 mainY = 10
 
 var quiz_num=8;
+var trials=8;
+var current_level = 1
+
 ball_size=40;
 txtSize = 16
 gameStarted=false;
@@ -31,6 +33,9 @@ gameStarted=false;
 imessage = ""
 state=10;
 
+
+games = 1
+wins = 0
 
 var w = window.innerWidth;
 var h = window.innerHeight;
@@ -45,36 +50,54 @@ function genPat(){
 
 // display the answer circles (hidden)
 function displayPat(){
+    fill(200)
+    text(" Correct Answers:", 300, 130)
+    gShiftX = 200;
+    gShiftY = 0;
+    row = 0;
+    col = 0;
     for(var i=0; i<quiz_num; i++){
-        pat[i]=new unit();
+        if(i%4 == 0){
+            row +=1
+            col = 0
+        }    
         pat[i].answer=mypat[i];
-        pat[i].x=pat_shift+x_shift;
-        pat[i].y=y_shift;
-        pat[i].y+=i*ball_size;
+        pat[i].x = gShiftX + x_shift;
+        pat[i].x +=col*ball_size;
+        pat[i].y = gShiftY + y_shift + row*ball_size;
         pat[i].display();
+        col +=1
     }   
+
 }
 
-// hiddes the answer circles (draws over them)
-function displayHiddenPat(){
-    for(var i=0; i<quiz_num; i++){
-        pat[i]=new unit();
-        pat[i].answer=3;
-        pat[i].x=pat_shift+x_shift;
-        pat[i].y=y_shift;
-        pat[i].y+=i*ball_size;
-        pat[i].display();
-    }   
-}
+// // hiddes the answer circles (draws over them)
+// function displayHiddenPat(){
+//     for(var i=0; i<quiz_num; i++){
+//         pat[i]=new unit();
+//         pat[i].answer=3;
+//         pat[i].x=pat_shift+x_shift;
+//         pat[i].y=y_shift;
+//         pat[i].y+=i*ball_size;
+//         pat[i].display();
+//     }   
+// }
 
 // display all the guessing circles
-function displayCells(){    
+function displayCells(){  
+    row = 0;  
+    col = 0;
     for(var i=0; i< quiz_num; i++){
+        if(i%4 == 0){
+            row +=1
+            col = 0
+        }
         cell[i].answer=mem[i];
-        cell[i].x=answer_shift+x_shift;
-        cell[i].y=y_shift;
-        cell[i].y+=i*ball_size;
+        cell[i].x = x_shift;
+        cell[i].x +=col*ball_size;
+        cell[i].y = y_shift + row*ball_size;
         cell[i].display();
+        col +=1
     }
 }
 
@@ -86,12 +109,12 @@ function gameStates(){
         fill(220);
         textSize(txtSize);
         text("Please Select Your Level:", 100, 50)
-        text("How to Play:", 140, 300)        
+        text("How to Play:", 140, 330)        
         mm = "Guess a hidden array of True or False \nchoices. You can peek your score for \na limited number of trials. To win: \n   - Use more Brain, less Luck! \n   - Learn from your past trials.\n   - Find an optimal strategy. \n\nAn article about optimal strategy at:"
-        ww = "www.brainxyz.com/blog/guessbetter"
-        text(mm, 50, 410)
+        ww = "www.brainxyz.com/blog/mineclear"
+        text(mm, 50, 430)
         fill(200,200,0)
-        text(ww, 50, 510)
+        text(ww, 50, 530)
     }
     else if(state==0){
         text("Guess T or F", 140, 20)
@@ -106,11 +129,12 @@ function gameStates(){
 function gameRestart(cstate){
     state=cstate
     gameStates();
-    trials=8;
+    //trials=8;
     hide=true;
     for(var i=0; i<quiz_num; i++){
         mem[i]=0;
         cell[i]=new unit();
+        pat[i] =new unit();
     }
     genPat();
     restart=new unitRestart();
@@ -123,8 +147,8 @@ function gameRestart(cstate){
     main.y=mainY;
 
     hideScore=new unitRect();
-    hideScore.x=20;
-    hideScore.y=200;
+    hideScore.x=135;
+    hideScore.y=80;
     hideScore.xWidth=90;
 }
 
@@ -139,10 +163,11 @@ function scoreShow(){
         }else if(checkAnswer() == quiz_num){
             state=1;
             imessage = ""
+            wins +=1
         }else{
             state=0;
             mytrials=trials + " trials\nremains!";
-            imessage = checkAnswer() + " of "+ quiz_num+ "\nare correct!\n" + "Not perfect!\nGuess Better!"
+            imessage = "Score: " + checkAnswer() + "/"+ quiz_num
         }
     }else{
         hide=true;
@@ -158,31 +183,22 @@ function myscore(myfinal){
     fill(200);
     textSize(txtSize);
     text(sc,30,225);
-    text(imessage, 30, 310)
+    text(imessage, 240, 110)
 }
 
-// display some text
-function recDisplay(){
-    fill(150)
-    sguess= "   your\nguesses"
-    sanswer=" hidden\nanswers"
-    text(sguess, 138,80)
-    text(sanswer, 210,80)
-}
+// // display some text
+// function recDisplay(){
+//     fill(150)
+//     sguess= "   your\nguesses"
+//     sanswer=" hidden\nanswers"
+//     text(sguess, 138,80)
+//     text(sanswer, 210,80)
+// }
 
-function add(a,b){ return a+b; }
+// function add(a,b){ return a+b; }
 
 // checks the current total score for the answers
 function checkAnswer(){
-
-    // prefinal=score.reduce(add,0);
-    // for(var i=0; i<quiz_num; i++){
-    //     if(mypat[i]==mem[i]){ score[i]=1;
-    //     }else{ score[i]=0; }
-    // }
-    
-    // final=score.reduce(add,0);
-    // return final;
 
     total = 0;
     for(var i=0; i<quiz_num; i++){
@@ -258,7 +274,8 @@ function unitRect(){
 function unitLevel(){
 
     this.levelName="";
-    this.qnum=7;
+    this.qnum = 8;
+    this.mTrials = 8;
     this.x=230+shift;
     this.y=100;
     this.xWidth=80;
@@ -314,19 +331,30 @@ function setup(){
     L1 = new unitLevel()
     L2 = new unitLevel()
     L3 = new unitLevel()
+    L4 = new unitLevel()
 
-    L1.levelName = "  Baby"
+    L1.levelName = "   Level 1"
     L1.qnum = 8;
+    L1.mTrials = 8;
     L1.x = 140
     L1.y = 80
-    L2.levelName = "  Amateur"
-    L2.qnum = 10; 
+    L2.levelName = "   Level 2"
+    L2.qnum = 12; 
+    L2.mTrials = 10;
     L2.x = 140
     L2.y = 140   
-    L3.levelName = "  Master"
-    L3.qnum = 12; 
+    L3.levelName = "   Level 3"
+    L3.qnum = 16; 
+    L3.mTrials = 11;
     L3.x = 140
-    L3.y = 200        
+    L3.y = 200    
+
+    L4.levelName = "   Level 4"
+    L4.qnum = 20; 
+    L4.mTrials = 12;
+    L4.x = 140
+    L4.y = 260   
+
 }
 
 function draw(){
@@ -336,18 +364,22 @@ function draw(){
         L1.display()
         L2.display()       
         L3.display()
+        L4.display()
 
      }else{
         myscore(checkAnswer());
         displayCells();
-        displayPat();
-        if(state==0){
-            displayHiddenPat()
-            recDisplay()
+
+        if(state == 1 || state == 2){
+            displayPat();
         }
-        mytrials="remaining \ntrials: "+" " + trials;
+        //if(state==0){
+            //displayHiddenPat()
+            //recDisplay()
+        //}
+        mytrials="trials: "+" " + trials;
         fill(220);
-        text(mytrials, 30,160);
+        text(mytrials,  restartX, 110);
 
        hideScore.display()
        restart.display();
@@ -355,6 +387,9 @@ function draw(){
        fill(220);
        text("restart", restartX,restartY+60);
        text("main", mainX, mainY+60);
+
+       text("Wins: " + wins, restartX, 400)
+       text("Games: " + games, restartX, 430)
     }
     gameStates();
 
@@ -372,40 +407,72 @@ function touchStarted(){
                 }   
             }
             displayCells();
-            displayPat();
-            checkAnswer();
-            if(state==0){
-                displayHiddenPat()
-                recDisplay()
+            if(state == 1 || state == 2){
+                displayPat();
             }
+            checkAnswer();
+            //if(state==0){
+                //displayHiddenPat()
+                //recDisplay()
+            //}
            scoreShow();
         }
 
        if(checkBoundery(restart)){
             // restarts with state = 0
             gameRestart(0);
+            games +=1
+
+            if(current_level == 1){
+                trials = L1.mTrials
+            }else if(current_level == 2){
+                trials = L2.mTrials
+            }else if(current_level == 3){
+                trials = L3.mTrials
+            }else if(current_level == 4){
+                trials = L3.mTrials
+            }
+
+
        }
 
         if(checkBoundery(main)){
             // back to main menu with state = 10
             gameRestart(10);
+            wins = 0
+            games = 1
        } 
 
        if(state==10){
 
             if(checkBounderyRect(L1)){
                 quiz_num = L1.qnum
+                trials = L1.mTrials
+                current_level = 1
                 state = 0
                 gameRestart(state)
             }
             if(checkBounderyRect(L2)){
                 quiz_num = L2.qnum
+                trials = L2.mTrials
+                current_level = 2
                 state = 0
                 gameRestart(state)
 
             }
             if(checkBounderyRect(L3)){
                 quiz_num = L3.qnum
+                trials = L3.mTrials
+                current_level = 3                
+                state = 0
+                gameRestart(state)
+
+            }
+
+            if(checkBounderyRect(L4)){
+                quiz_num = L4.qnum
+                trials = L4.mTrials
+                current_level = 4                
                 state = 0
                 gameRestart(state)
 
